b073045f57f857ba7136dbd25ef5bab26f82dc8f
vesnud <vesnud@gmail.ru>
2023-01-30 16:44:03 +0300

    маштаб изменение

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import sys
 import requests
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel
+from PyQt5.QtCore import Qt
 
 SCREEN_SIZE = [600, 450]
 
@@ -11,11 +12,13 @@ SCREEN_SIZE = [600, 450]
 class Example(QWidget):
     def __init__(self):
         super().__init__()
+        self.x, self.y = 137.284199, -26.7280478
+        self.spn = 20
         self.getImage()
         self.initUI()
 
     def getImage(self):
-        map_request = "http://static-maps.yandex.ru/1.x/?ll=137.284199,-26.7280478&spn=34,34&l=sat"
+        map_request = f"http://static-maps.yandex.ru/1.x/?ll={self.x},{self.y}&spn=10,{int(self.spn)}&l=sat"
         response = requests.get(map_request)
 
         if not response:
@@ -33,12 +36,20 @@ class Example(QWidget):
         self.setGeometry(100, 100, *SCREEN_SIZE)
         self.setWindowTitle('Отображение карты')
 
+
         ## Изображение
         self.pixmap = QPixmap(self.map_file)
         self.image = QLabel(self)
         self.image.move(0, 0)
         self.image.resize(600, 450)
         self.image.setPixmap(self.pixmap)
+
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key_PgUp:
+            self.spn += 1
+
+        elif event.key() == Qt.Key_PageDown:
+            self.spn -= 1
 
     def closeEvent(self, event):
         """При закрытии формы подчищаем за собой"""
